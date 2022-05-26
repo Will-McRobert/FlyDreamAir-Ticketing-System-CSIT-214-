f28e530786f0b9aeb5aba032d9f19853e2b3b37d
Will-McRobert <104036443+Will-McRobert@users.noreply.github.com>
2022-05-26 16:17:10 +1000

    Update urls.py
    Add URL paths for the different site views.

--- store/urls.py
+++ store/urls.py
@@ -2,6 +2,13 @@ from django.urls import path
 
 from . import views
 
+# Site URL Paths
+
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('flight-select', views.flight_select, name='flight_select'),
+    path('seat-select', views.seat_select, name='seat_select'),
+    path('service-select', views.service_select, name='service_select'),
+    path('user-payment', views.user_payment, name='user_payment'),
+    path('ticket-confirmation', views.ticket_confirmation, name='ticket_confirmation')
 ]
